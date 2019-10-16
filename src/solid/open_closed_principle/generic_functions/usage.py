8fa3e26fbdbadab81b8a916d94f843c4d25ee703
Israel Roldan <israel.alberto.rv@gmail.com>
2019-10-15 20:12:07 -0500

    Added an empty file for start to implement the generic functions principle.

---
+++ src/solid/open_closed_principle/generic_functions/usage.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+SOLID
+
+Open/Closed Principle: Mix-ins.
+Software entities ... should be open for extension, but closed for modification..
+
+Overloading/Generic Functions
+
+References:
+    https://www.python.org/dev/peps/pep-3124/#overloading-generic-functions
+    https://julien.danjou.info/python-3-4-single-dispatch-generic-function
+"""
+
+assert True == False
